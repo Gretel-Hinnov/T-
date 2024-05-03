b882b884e5180ffc4263d8e06be1409e179b5d81
Opilane TTHK <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-05-03 12:03:00 +0300

    ForLoop

---
+++ ForLoop/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int val = 10;
+                Console.Write("Sisesta ridade arv : ");
+                val = Convert.ToInt32(Console.ReadLine());
+                int i, j, k;
+                for (i = 1; i <= val; i++)
+                {
+                    for (j = 1; j <= val - i; j++)
+                    {
+                        // Console.Write("");  
+                    }
+                    for (k = 1; k <= i; k++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine("htthtg");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
